d6dabb8e3d57a14e7b1ed11758048e6927be4a06
Lutfur Rahman Tanvir <lutfurrahmantanvir@gmail.com>
2026-06-21 14:50:19 +0600

    project initialization

---
+++ addons/nn_fund_management/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': "NN Fund Management",
+    'version': '1.0',
+    'category': 'Accounting',
+    'depends': ['base','web','website'],
+    'author': "Lutfur Rahman Tanvir",
+    'description': """
+    Description text
+    """,
+    # data files always loaded at installation
+    'data': [
+        'views/mymodule_view.xml',
+    ],
+    # data files containing optionally loaded demonstration data
+    'demo': [
+        'demo/demo_data.xml',
+    ],
+}
